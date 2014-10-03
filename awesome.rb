d67d9ab79498e0d56adf56381ce915cadd2a240c
Yannick <yannick.schutz@gmail.com>
2014-10-04 00:27:47 +0200

    DRING DRING

---
+++ awesome.rb
@@ -0,0 +1,30 @@
+require "json"
+require "twilio-ruby"
+class Awesome < Sinatra::Base
+
+  get "/" do
+    erb :index
+  end
+
+  post "/" do
+
+    # To find these visit https://www.twilio.com/user/account
+    account_sid = ENV["ACCOUNT_SID"]
+    auth_token = ENV["ACCOUNT_AUTH_TOKEN"]
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @call = @client.account.calls.create({:to => "9991231234",
+                                  :from => ENV["ACCOUNT_PHONE_NUMBER"],
+                                                                :url => "http://foo.com/call.xml"})
+
+  end
+
+  get "/:name" do
+    content_type 'application/xml'
+    "<?xml version='1.0' encoding='UTF-8'?>
+      <Response>
+        <Say voice='woman'>You are awesome, #{params[:name]}.</Say>
+      <Record maxLength='20' />
+    </Response>"
+  end
+end
